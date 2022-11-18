c870d6561f57522fdbf1d9686d5b91e8fe805a8d
raphael.kieling <raphael.kieling@telus.com>
2022-11-18 12:13:08 -0300

    Release 0.0.7

--- scripts/tag-new-version.bash
+++ scripts/tag-new-version.bash
@@ -1,11 +1,17 @@
+#!/bin/bash
+
+set -x
+
 # Set next version number
 export RELEASE=$1
 
+FINAL_VERSION="${RELEASE:1}"
+
 # Create tags
-poetry version $1
+poetry version $FINAL_VERSION
 git add -A
-git commit --allow-empty -m "Release $RELEASE"
-git tag -a $RELEASE -m "Version $RELEASE"
+git commit --allow-empty -m "Release $FINAL_VERSION"
+git tag -a $RELEASE -m "Version $FINAL_VERSION"
 
 # Push
 git push origin main
